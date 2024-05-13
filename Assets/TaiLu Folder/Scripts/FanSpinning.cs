57ee1ae81383b8932deb3c4ae7ce91a48a657fff
YukiCodingW <147054821+YukiCodingW@users.noreply.github.com>
2024-05-13 17:49:58 +0800

    Update to Fan Script
    Update to Fan Script

--- Assets/TaiLu Folder/Scripts/FanSpinning.cs
+++ Assets/TaiLu Folder/Scripts/FanSpinning.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.Video;
 
 public class FanSpinning : MonoBehaviour
 {
     bool spinning = false;
+    float speed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,12 +24,26 @@ public class FanSpinning : MonoBehaviour
         }
         else if (spinning == false)
         {
-
+            SpinToStop();
         }
     }
     public void Spin()
     {
-        transform.Rotate(0f, 300f * Time.deltaTime, 0f, Space.Self);
+        speed = 0f;
+        while (speed <= 300f)
+        {
+            speed += speed + 30f;
+            transform.Rotate(0f, speed * Time.deltaTime, 0f, Space.Self);
+        }
+    }
+
+    public void SpinToStop()
+    {
+        while (speed >= 300f && speed != 0f)
+        {
+            speed += speed - 30f;
+            transform.Rotate(0f, speed * Time.deltaTime, 0f, Space.Self);
+        }
     }
 
     public void ToggleStartStop()
