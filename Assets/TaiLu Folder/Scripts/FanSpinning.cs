c45c989ec66ce169bd1313fd82caacd2c721aaa3
YukiCodingW <147054821+YukiCodingW@users.noreply.github.com>
2024-05-14 00:14:48 +0800

    Fan
    Fan

--- Assets/TaiLu Folder/Scripts/FanSpinning.cs
+++ Assets/TaiLu Folder/Scripts/FanSpinning.cs
@@ -2,13 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering;
 using UnityEngine.Video;
 
 public class FanSpinning : MonoBehaviour
 {
     bool spinning = false;
-    float Maxspeed;
+    float MaxSpeed;
     float MinSpeed = 0f;
+    float AngleX = 0f; 
+    float AngleZ = 0f;
+    bool test = false;
 
     // Start is called before the first frame update
     void Start()
@@ -22,26 +26,30 @@ public class FanSpinning : MonoBehaviour
         if (spinning == true)
         {
             StartCoroutine(Spin());
+            test = true; 
         }
         else if (spinning == false)
         {
-            StartCoroutine(SpinToStop());
+            if (test == true)
+            {
+                StartCoroutine(SpinToStop());
+            }
         }
     }
     IEnumerator Spin()
     {
-        for (Maxspeed = 0f; Maxspeed < 15f; Maxspeed+= 0.1f)
+        for (MaxSpeed = 0f; MaxSpeed < 20f; MaxSpeed+= 0.5f)
         {
-            transform.Rotate(0f, Maxspeed * Time.deltaTime, 0f, Space.Self);
+            transform.Rotate(AngleX, MaxSpeed * Time.deltaTime, AngleZ, Space.Self);
             yield return new WaitForSeconds(.1f);
         }
     }
 
     IEnumerator SpinToStop()
     {
-        for (MinSpeed = Maxspeed; MinSpeed >= 0; MinSpeed-= 0.1f)
+        for (MinSpeed = MaxSpeed; MinSpeed >= 0f; MinSpeed-= 0.5f)
         {
-            transform.Rotate(0f, MinSpeed * Time.deltaTime, 0f, Space.Self);
+            transform.Rotate(AngleX, MinSpeed * Time.deltaTime, AngleZ, Space.Self);
             yield return new WaitForSeconds(.1f);
         }
     }
